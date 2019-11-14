4eab361e4061fd42f9f8ca157b81779ff2310f96
chunleidu100 <chunleidu100@gmail.com>
2019-11-14 14:34:21 -0500

    added findbyTradeId

--- src/main/java/com/fmr/java8/case_study/rest/TraderAdminController.java
+++ src/main/java/com/fmr/java8/case_study/rest/TraderAdminController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,4 +26,13 @@ public class TraderAdminController {
             traderRepository.save(trader),
             HttpStatus.CREATED);
     }
+    
+    
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getTraderById(@RequestParam(value="traderId")String tradeId) {
+        return new ResponseEntity(
+            traderRepository.findByTraderId(tradeId),
+            HttpStatus.CREATED);
+    }
+    
 }
